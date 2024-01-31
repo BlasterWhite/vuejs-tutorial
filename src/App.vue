56271e46d27ed2f235ab5339b7a4985ae9995d58
BlasterWhite <icarambala@gmail.com>
2024-01-31 17:01:28 +0100

    Create Counter 🧾

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
-<template></template>
+<script setup>
+import MyCounter from "@/components/MyCounter.vue";
+import {ref} from "vue";
 
-<script setup></script>
+const count = ref(0);
+</script>
 
-<style scoped></style>
+<template>
+  <div class="app">
+    <h1>Vue 3 Composition API</h1>
+    <p>Count is: {{ count }}</p>
+    <my-counter v-model="count" />
+    <p v-if="count < 10">⏬ 10</p>
+    <p v-else-if="count === 10">⏩ 10</p>
+    <p v-else-if="count > 10">⏫ 10</p>
+  </div>
+</template>
+
+
+<style scoped>
+  .app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+}
+</style>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref} from "vue";
+
+const emits = defineEmits(["increment"]);
+
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    required: true,
+  },
+})
+
+const count = ref(props.modelValue);
+
+function increment() {
+  count.value++;
+  emits("update:modelValue", count.value);
+}
+</script>
+
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
+
+<style scoped>
+  button {
+  background-color: #eee;
+  border: none;
+  border-radius: 4px;
+  color: #333;
+  font-size: 1.5em;
+  padding: 0.5em 1em;
+}
+</style>
