cb4ba493ca443b2e7eb0ed2aea2e635d061aee50
maximefurious <93995791+maximefurious@users.noreply.github.com>
2024-02-07 11:02:16 +0100

    Initial

---
+++ tutorial/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+const count = 10;
+const shoppingList = ["Apple", "Meat", "Pencils"]
+</script>
+
+<template>
+  <div>
+    <h1>Hello World !</h1>
+    <p v-if="count < 10">⏬ Less than 10</p>
+    <p v-else-if="count === 10">⏩ Equal 10</p>
+    <p v-else>⏫ More than 10</p>
+    <span v-for="item in shoppingList"> {{ item }}</span>
+  </div>
+
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+h1 {
+  color: green;
+  font-style: oblique;
+}
+</style>
